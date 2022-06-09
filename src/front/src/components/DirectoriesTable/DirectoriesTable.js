aeb0a1651e10e9c5aae6ff60215025f7cc137cea
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2022-06-09 18:58:21 -0500

    Provide copy and Storage Explorer UI commands

--- src/front/src/components/DirectoriesTable/DirectoriesTable.js
+++ src/front/src/components/DirectoriesTable/DirectoriesTable.js
@@ -5,87 +5,94 @@ import DetailsIcon from '@mui/icons-material/InfoTwoTone'
 import EditIcon from '@mui/icons-material/EditOutlined'
 import DirectoriesTableMembers from './DirectoriesTableMembers'
 import './DirectoriesTable.css'
+import StorageExplorerIcon from '../../images/storage-explorer.svg'
+import IconButton from "@mui/material/IconButton"
 
 export const DirectoriesTable = ({ data, onDetails, onEdit, strings }) => {
-    return (
-        <Table striped bordered hover className='directoriesTable'>
-            <thead>
-                <tr>
-                    <th>
-                        {strings.folderLabel}
-                    </th>
-                    <th>
-                        {strings.spaceUsedLabel}
-                    </th>
-                    <th>
-                        {strings.monthlyCostLabel}
-                    </th>
-                    <th>
-                        {strings.whoHasAccessLabel}
-                    </th>
-                    <th>
-                        {strings.fundCodeLabel}
-                    </th>
-                    <th>
-                        {strings.actionsLabel}
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map(row => {
-                    return (
-                        <tr key={row.name}>
-                            <td className='name'>
-                                {row.name}
-                            </td>
-                            <td className='spaceused'>
-                                {row.size}
-                            </td>
-                            <td className='costs'>
-                                {row.cost}
-                            </td>
-                            <td className='owner'>
-                                <DirectoriesTableMembers members={row.userAccess} strings={strings} />
-                            </td>
-                            <td className='fundcode'>
-                                {row.fundCode}
-                            </td>
-                            <td className='actions'>
-                                {onEdit && <EditIcon onClick={() => onEdit(row)} className='action' />}
-                                {onDetails && <DetailsIcon onClick={() => onDetails(row)} className='action' />}
-                            </td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </Table>
-    )
+	return (
+		<Table striped bordered hover className='directoriesTable'>
+			<thead>
+				<tr>
+					<th>
+						{strings.folderLabel}
+					</th>
+					<th>
+						{strings.spaceUsedLabel}
+					</th>
+					<th>
+						{strings.monthlyCostLabel}
+					</th>
+					<th>
+						{strings.whoHasAccessLabel}
+					</th>
+					<th>
+						{strings.fundCodeLabel}
+					</th>
+					<th>
+						{strings.actionsLabel}
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				{data.map(row => {
+					return (
+						<tr key={row.name}>
+							<td className='name'>
+								{row.name}
+							</td>
+							<td className='spaceused'>
+								{row.size}
+							</td>
+							<td className='costs'>
+								{row.cost}
+							</td>
+							<td className='owner'>
+								<DirectoriesTableMembers members={row.userAccess} strings={strings} />
+							</td>
+							<td className='fundcode'>
+								{row.fundCode}
+							</td>
+							<td className='actions'>
+								{onEdit && <EditIcon onClick={() => onEdit(row)} className='action' />}
+								{onDetails && <DetailsIcon onClick={() => onDetails(row)} className='action' />}
+								<IconButton aria-label={strings.openInStorageExplorerLabel} size='small' onClick={() => { window.open(row.storageExplorerURI); return false }}>
+									<img src={StorageExplorerIcon} title={strings.openInStorageExplorerLabel} />
+								</IconButton>
+							</td>
+						</tr>
+					)
+				})}
+			</tbody>
+		</Table>
+	)
 }
 
 DirectoriesTable.propTypes = {
-    data: PropTypes.array,
-    onDetails: PropTypes.func,
-    onEdit: PropTypes.func,
-    strings: PropTypes.shape({
-        actionsLabel: PropTypes.string,
-        folderLabel: PropTypes.string,
-        fundCodeLabel: PropTypes.string,
-        monthlyCostLabel: PropTypes.string,
-        spaceUsedLabel: PropTypes.string,
-        whoHasAccessLabel: PropTypes.string,
-    })
+	data: PropTypes.array,
+	onDetails: PropTypes.func,
+	onEdit: PropTypes.func,
+	strings: PropTypes.shape({
+		actionsLabel: PropTypes.string,
+		folderLabel: PropTypes.string,
+		fundCodeLabel: PropTypes.string,
+		monthlyCostLabel: PropTypes.string,
+		spaceUsedLabel: PropTypes.string,
+		whoHasAccessLabel: PropTypes.string,
+		openInStorageExplorerLabel: PropTypes.string
+	})
 }
 
 DirectoriesTable.defaultProps = {
-    data: [],
-    strings: {
-        actionsLabel: 'Actions',
-        folderLabel: 'Folder',
-        fundCodeLabel: 'Fund Code',
-        monthlyCostLabel: 'Monthly Cost',
-        spaceUsedLabel: 'Space Used',
-        whoHasAccessLabel: 'Who Has Access?',
-    }
+	data: [],
+	strings: {
+		actionsLabel: 'Actions',
+		folderLabel: 'Folder',
+		fundCodeLabel: 'Fund Code',
+		monthlyCostLabel: 'Monthly Cost',
+		spaceUsedLabel: 'Space Used',
+		whoHasAccessLabel: 'Who Has Access?',
+		openInStorageExplorerLabel: 'Open in Storage Explorer'
+	}
 }
 
 export default DirectoriesTable
--- src/front/src/components/DirectoryDetails/DirectoryDetails.js
+++ src/front/src/components/DirectoryDetails/DirectoryDetails.js
@@ -1,83 +1,113 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Grid from '@mui/material/Grid'
+import IconButton from "@mui/material/IconButton"
 import './DirectoryDetails.css'
 import DirectoriesTableMembers from '../DirectoriesTable/DirectoriesTableMembers'
+import CopyIcon from '@mui/icons-material/ContentCopy';
+import FilledCopyIcon from '@mui/icons-material/ContentCopyTwoTone';
+import StorageExplorerIcon from '../../images/storage-explorer.svg'
 
 const DirectoryDetails = ({ data, strings }) => {
 
-    const format = value => {
+	// The state of the Copy command icon
+	const [copied, setCopied] = useState(false)
+
+	const format = value => {
 		if (Array.isArray(value))
 			return value.join(", ");
-        return value ? value : '-';
-    }
+		return value ? value : '-';
+	}
 
-    return (
-        <Grid container className='directoryDetails'>
-            <Grid item xs={12} className='title'>
-            <div className='label'>{strings.folderLabel}:</div> {data.name}
-            </Grid>
-            <Grid item md={4} sm={6} xs={12} className='detail'>
-                <div className='label'>{strings.sizeLabel}:</div> {format(data.size)}
-            </Grid>
-            <Grid item md={4} sm={6} xs={12} className='detail'>
-                <div className='label'>{strings.costLabel}:</div> {format(data.cost)}
-            </Grid>
-            <Grid item md={4} sm={6} xs={12} className='detail'>
-                <div className='label'>{strings.fundCodeLabel}:</div> {format(data.fundCode)}
-            </Grid>
+	const handleCopyUrl = (url) => {
+		// Copy the URL to the clipboard
+		navigator.clipboard.writeText(url)
+		// Change the icon for three seconds to provide visual feedback
+		setCopied(true)
+		let timer = setInterval(() => { unsetCopied(timer) }, 3000)
+	}
+
+	const unsetCopied = (timer) => {
+		setCopied(false)
+		clearInterval(timer)
+	}
+
+	return (
+		<Grid container className='directoryDetails'>
+			<Grid item xs={12} className='title'>
+				<div className='label'>{strings.folderLabel}:</div>{data.name}
+			</Grid>
 			<Grid item md={4} sm={6} xs={12} className='detail'>
-                <div className='label'>{strings.accessTierLabel}:</div> {format(data.accessTier)}
-            </Grid>
+				<div className='label'>{strings.sizeLabel}:</div>{format(data.size)}
+			</Grid>
 			<Grid item md={4} sm={6} xs={12} className='detail'>
-                <div className='label'>{strings.createdLabel}:</div> {format(data.createdOn)}
-            </Grid>
-            <Grid item md={4} sm={6} xs={12} className='detail'>
-                <div className='label'>{strings.ownerLabel}:</div> {format(data.owner)}
-            </Grid>
+				<div className='label'>{strings.costLabel}:</div>{format(data.cost)}
+			</Grid>
+			<Grid item md={4} sm={6} xs={12} className='detail'>
+				<div className='label'>{strings.fundCodeLabel}:</div>{format(data.fundCode)}
+			</Grid>
+			<Grid item md={4} sm={6} xs={12} className='detail'>
+				<div className='label'>{strings.accessTierLabel}:</div>{format(data.accessTier)}
+			</Grid>
+			<Grid item md={4} sm={6} xs={12} className='detail'>
+				<div className='label'>{strings.createdLabel}:</div>{format(data.createdOn)}
+			</Grid>
+			<Grid item md={4} sm={6} xs={12} className='detail'>
+				<div className='label'>{strings.ownerLabel}:</div>{format(data.owner)}
+			</Grid>
 			<Grid item xs={12} className='detail'>
-                <div className='label'>{strings.userAccessLabel}:</div>
-					<DirectoriesTableMembers members={data.userAccess} strings={strings} />
-            </Grid>
-            <Grid item xs={12} className='detail'>
-                <div className='label'>{strings.uriLabel}:</div> {format(data.uri)}
-            </Grid>
-        </Grid>
-    )
+				<div className='label'>{strings.userAccessLabel}:</div>
+				<DirectoriesTableMembers members={data.userAccess} strings={strings} />
+			</Grid>
+			<Grid item xs={12} className='detail'>
+				<div className='label'>{strings.uriLabel}:</div><span>{format(data.uri)}</span>
+				<IconButton aria-label={strings.copyToClipboardLabel} onClick={() => handleCopyUrl(data.uri)} color={copied ? 'success' : 'secondary'} size='small'>
+					{copied ? <FilledCopyIcon /> : <CopyIcon />}
+				</IconButton>
+				<IconButton aria-label={strings.openInStorageExplorerLabel} size='large' onClick={() => { window.open(data.storageExplorerURI); return false }}>
+					<img src={StorageExplorerIcon} title={strings.openInStorageExplorerLabel} />
+				</IconButton>
+			</Grid>
+		</Grid>
+	)
 }
 
 DirectoryDetails.propTypes = {
-    data: PropTypes.object,
-    strings: PropTypes.shape({
-        accessTierLabel: PropTypes.string,
-        costLabel: PropTypes.string,
-        createdLabel: PropTypes.string,
-        folderLabel: PropTypes.string,
-        fundCodeLabel: PropTypes.string,
-        ownerLabel: PropTypes.string,
+	data: PropTypes.object,
+	strings: PropTypes.shape({
+		accessTierLabel: PropTypes.string,
+		costLabel: PropTypes.string,
+		createdLabel: PropTypes.string,
+		folderLabel: PropTypes.string,
+		fundCodeLabel: PropTypes.string,
+		ownerLabel: PropTypes.string,
 		userAccessLabel: PropTypes.string,
-        regionLabel: PropTypes.string,
-        sizeLabel: PropTypes.string,
-        totalFilesLabel: PropTypes.string,
-        uriLabel: PropTypes.string,
-    })
+		regionLabel: PropTypes.string,
+		sizeLabel: PropTypes.string,
+		totalFilesLabel: PropTypes.string,
+		uriLabel: PropTypes.string,
+		openInStorageExplorerLabel: PropTypes.string,
+		copyToClipboardLabel: PropTypes.string
+	})
 }
 
 DirectoryDetails.defaultProps = {
-    data: {},
-    strings: {
-        accessTierLabel: 'Storage type',
-        costLabel: 'Monthly cost',
-        createdLabel: 'Created on',
-        folderLabel: 'Folder',
-        fundCodeLabel: 'Fund code',
-        ownerLabel: 'Owner',
+	data: {},
+	strings: {
+		accessTierLabel: 'Storage type',
+		costLabel: 'Monthly cost',
+		createdLabel: 'Created on',
+		folderLabel: 'Folder',
+		fundCodeLabel: 'Fund code',
+		ownerLabel: 'Owner',
 		userAccessLabel: 'User Access',
-        regionLabel: 'Region',
-        sizeLabel: 'Total size',
-        totalFilesLabel: 'Total files',
-        uriLabel: 'URL',
-    }
+		regionLabel: 'Region',
+		sizeLabel: 'Total size',
+		totalFilesLabel: 'Total files',
+		uriLabel: 'URL',
+		openInStorageExplorerLabel: 'Open in Storage Explorer',
+		copyToClipboardLabel: 'Copy to clipboard'
+	}
 }
 
 export default DirectoryDetails
